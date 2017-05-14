399afbd5e9e5acda4b7a6aab5a0428d2ec2d3448
medandrew <359747@mail.ru>
2017-05-15 01:19:04 +0700

    Домашнее задание 4

--- 4/upload.php
+++ 4/upload.php
@@ -3,7 +3,13 @@
 if (isset($_FILES['userFile'])) {
     if (0 == $_FILES['userFile']['error']) {
         if ($_FILES['userFile']['type'] == 'image/jpeg' || $_FILES['userFile']['type'] == 'image/png') {
-            move_uploaded_file ($_FILES['userFile']['tmp_name'], __DIR__ . '/uploads/' . $_FILES['userFile']['name']);
+            $filename = $_FILES['userFile']['name'];
+            $i = 0;
+            while (file_exists(__DIR__ . '/uploads/' . $filename)) {
+                $filename = $i . '_' . $_FILES['userFile']['name'];
+                $i++;
+            }
+            move_uploaded_file ($_FILES['userFile']['tmp_name'], __DIR__ . '/uploads/' . $filename);
             //header('Location: /files.php');
 ?>
 
