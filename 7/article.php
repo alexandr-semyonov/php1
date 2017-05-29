d794b6bc7fa13d2df31237cd552437a96ca658cc
Andrew <359747@mail.ru>
2017-05-29 21:08:47 +0700

    Домашнее задание 7. Исправление

--- 7/article.php
+++ 7/article.php
@@ -2,8 +2,13 @@
 require __DIR__ . '/classes/News.php';
 require __DIR__ . '/classes/View.php';
 
-$news = new News(__DIR__ . '/data/news.txt');
+if (isset($_GET['id']) && (int)($_GET['id']) > 0){
+    $id = (int)$_GET['id'];
 
-$view = new View();
+    $article = (new News(__DIR__ . '/data/news.txt'))->getById($id);
 
-$view->assign('News', $news)->display(__DIR__ . '/templates/Article.html');
+    $view = new View();
+
+    $view->assign('Article', $article)->display(__DIR__ . '/templates/Article.html');
+}
+die;
