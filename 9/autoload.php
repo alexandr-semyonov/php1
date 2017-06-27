d37a4f86db337c1d6d3a6cca4dfa424213719bef
Andrew <359747@mail.ru>
2017-06-27 23:49:04 +0700

    Домашнее задание 9

--- 9/autoload.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-function __autoload($class){
-    if (0 === strpos($class, 'App')){
-        $name = str_replace('\\', '/', $class);
-        require __DIR__ . '/' . $name . '.php';
-    }
-}
